8c97648bd96cd9b3bc65c6c4ee51993b77ded939
Burke Ramsey <birk5437@gmail.com>
2015-12-24 14:15:09 -0700

    HACK: Every user is an admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ActiveRecord::Base
 
   has_many :items, as: :created_by, foreign_key: :created_by_id, dependent: :nullify
   belongs_to :created_by, :class_name => "User"
+
+  before_save :set_admin_to_true
+
+  def set_admin_to_true
+    self.admin = true
+  end
 end
